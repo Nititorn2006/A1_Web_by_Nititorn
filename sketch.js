98893f17016eb5d8e656995b7f50a3f96b97579f
Nititorn2006 <nititornwongsuwan2006@gmail.com>
2025-09-08 18:37:24 +0700

    Start button added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(25, 20, 23);
+  startButton = createButton('START');
+  startButton.position(width / 2, height / 2);
+  startButton.mousePressed(startGame);
+  startButton.class('start-button');
 }
 
-function draw() {
-  background(0, 10, 30, 25);
-}
+  let startButton;
+  let gameStarted = false;
+
+
+  function startGame() {
+    gameStarted = true;
+    startButton.hide();
+  }
+
+  function draw() {
+    if (gameStarted == true) {
+      line(mouseX, mouseY, pmouseX, pmouseY);
+      stroke(255, 0, 0);
+    }
+  }
