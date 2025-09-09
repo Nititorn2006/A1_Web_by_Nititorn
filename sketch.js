6eaa1bd0bb72bbe4f8875d5844313ad15390976b
Nititorn2006 <nititornwongsuwan2006@gmail.com>
2025-09-09 09:11:40 +0700

    Paddle draw on click

--- sketch.js
+++ sketch.js
@@ -1,24 +1,44 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(25, 20, 23);
+  
   startButton = createButton('START');
   startButton.position(width / 2, height / 2);
   startButton.mousePressed(startGame);
   startButton.class('start-button');
+  leftX = 50;        
+  rightX = width - 50;
+  leftY = height / 2;
+  rightY = height / 2;
 }
 
   let startButton;
   let gameStarted = false;
-
+  let player1;
+  let leftX, rightX;
+  let leftY, rightY;
 
   function startGame() {
     gameStarted = true;
     startButton.hide();
   }
 
+  function drawLeftPaddle(x, y) {
+    stroke(255);
+    strokeWeight(4);
+    line(x, y - 50, x, y + 50);
+  }
+
+  function drawRightPaddle(x, y) {
+    stroke(255);
+    strokeWeight(4);
+    line(x, y - 50, x, y + 50);
+  }
+
   function draw() {
     if (gameStarted == true) {
-      line(mouseX, mouseY, pmouseX, pmouseY);
-      stroke(255, 0, 0);
+      background(25, 20, 23);
+      drawLeftPaddle(leftX, mouseY);
+      drawRightPaddle(rightX, rightY);
     }
   }
